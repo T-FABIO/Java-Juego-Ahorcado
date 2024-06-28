b8c64e28a183c7e7c604974f5aff19ad54cc20ff
T-FABIO <fabiotru04@gmail.com>
2024-06-27 21:53:07 -0500

    :)

--- src/Ahoracado.java
+++ src/Ahoracado.java
@@ -27,12 +27,12 @@ public class Ahoracado {
         while (!palabraAdivinada  && intentos < intentosMaximos) {
             System.out.println("Palabra a adivinar: " + String.valueOf(letrasAdivinadas) + " (" + palabraSecreta.length() + " letras).");
 
-            System.out.print("Introduce una letra, porfavor: ");
-
             // Usamos clase scanner para pedir una letra
+            System.out.print("Introduce una letra, porfavor: ");
             char letra = Character.toLowerCase(scanner.next().charAt(0));
 
             boolean letraCorrecta = false;
+
             for (int i = 0; i < palabraSecreta.length(); i++) {
                 // Estructura de control condicional
                 if(palabraSecreta.charAt(i) == letra){
@@ -55,7 +55,7 @@ public class Ahoracado {
         }
         
         if (!palabraAdivinada) {
-            System.out.println("!Te has quedado sin intentos has perdido¡");
+            System.out.println("!Te has quedado sin intentos has perdido¡");      
         }
 
         scanner.close();
